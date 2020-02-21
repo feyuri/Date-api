912d91ff076d153626ac426c1fcb6698e6672935
feyuri <yferreira30@gmail.com>
2020-02-21 13:07:35 -0300

    autenticação - começo da implementação do CRUD.

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -4,10 +4,39 @@ const User = require('../models/user');
 module.exports = {
     async store(req, res, next) {
         try {
-            console.log('aqui')
-            console.log(req.body.data)
-            res.json(true)
-        } catch{ }
+            let { name, email, number, password } = req.body
+            let user = await Users.findOne({ name, email, nunber, password })
+            if (!user) {
+                let user = await new user(req.body)
+                user.save().then(aluno => {
+                    resp.json(aluno)
+                })
+            } else {
+                resp.json(user)
+            }
 
-    }
+
+        } catch{
+            res.status(400)
+            res.json({ message: error.message })
+
+        }
+
+    },
+    async indexedDB(req, resp, next) {
+        try {
+            User.find().then(users => {
+                resp.json(users)
+                return next()
+            })
+
+        } catch{
+            res.status(404)
+            res.json({ message: 'not found' })
+
+
+        }
+
+
+    },
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,5 +2,20 @@ const userController = require('../controller/usersController');
 
 module.exports = server => {
     server.post('/new/user', userController.store)
+    server.get('/user', userController.indexedDB)
+
+    server.get('/users/:id', (req, resp, next) => {
+        User.findById(req.params.id).then(user => {
+            if (aluno) {
+                resp.json(user)
+            } else {
+                resp.status(404)
+                resp.json({ message: 'notfound' })
+            }
+        })
+    })
+
+
+
 }
 
